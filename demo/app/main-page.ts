61ebbcb6be28cdab06f39dc7f78fbf467c480ef2
Stefan Jongejan <s.jongejan@webuildapps.com>
2018-04-23 09:43:37 +0200

    Renamed HelloWorldModel to MainViewModel and moved the class to main-view-model.ts

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,83 +1,20 @@
-import * as pageobservable from 'tns-core-modules/data/observable';
-import {Observable} from 'tns-core-modules/data/observable';
+import * as observable from 'tns-core-modules/data/observable';
 import * as pages from 'tns-core-modules/ui/page';
-import {Parley, ParleyListener, ParleySslPinningListener, ParleyView} from "nativescript-parley";
+import {MainViewModel} from "./main-view-model";
+import {Page} from "tns-core-modules/ui/page";
+import {ParleyView} from "nativescript-parley";
 
-// Event handler for Page 'loaded' event attached in main-page.xml
-export function pageLoaded(args: pageobservable.EventData) {
-    // Get the event sender
-    let page = <pages.Page>args.object;
+let initialised = false;
 
-    let helloWorldModel = new HelloWorldModel();
-    helloWorldModel.setPage(page);
-    page.bindingContext = helloWorldModel;
-}
+export function pageLoaded(args: observable.EventData) {
+    if (initialised) return;
 
-export class HelloWorldModel extends Observable implements ParleyListener, ParleySslPinningListener {
+    let page: Page = <pages.Page>args.object;
+    let parleyView: ParleyView = page.getViewById("parleyView");
 
-    private page: pages.Page;
+    let mainViewModel = new MainViewModel();
+    mainViewModel.setParleyView(parleyView);
+    page.bindingContext = mainViewModel;
 
-    constructor() {
-        super();
-
-        this.set("loading", true);
-
-        Parley.init(this, "0W4qcE5aXoKq9OzvHxj2");
-        Parley.getInstance().registerUser("ZGFhbnw5ZTA5ZjQ2NWMyMGNjYThiYjMxNzZiYjBhOTZmZDNhNWY0YzVlZjYzMGVhNGZmMWUwMjFjZmE0NTEyYjlmMDQwYTJkMTJmNTQwYTE1YmUwYWU2YTZjNTc4NjNjN2IxMmRjODNhNmU1ODNhODhkMmQwNzY2MGYxZTEzZDVhNDk1Mnw1ZDcwZjM5ZTFlZWE5MTM2YmM3MmIwMzk4ZDcyZjEwNDJkNzUwOTBmZmJjNDM3OTg5ZWU1MzE5MzdlZDlkYmFmNTU1YTcyNTUyZWEyNjllYmI5Yzg5ZDgyZGQ3MDYwYTRjZGYxMzE3NWJkNTUwOGRhZDRmMDA1MTEzNjlkYjkxNQ"); // Optional
-        Parley.getInstance().setBaseUrl("https://www.irischat.com"); // Optional
-        Parley.getInstance().setBasePath("/clientApi/v1.1/"); // Optional
-        Parley.getInstance().setOfflineMessagingEnabled(false); // Optional
-        Parley.getInstance().enableSslPinning(this , "N9YyJf13LbHgGv1kn9/YEXGFLJbleikrcpDORa896ok=", "aR6DUqN8qK4HQGhBpcDLVnkRAvOHH1behpQUU1Xl7fE="); // Optional
-        Parley.getInstance().configure();
-
-        // Example for deregister for example, when using a logout button
-        // setTimeout(function () {
-        //     Parley.getInstance().deregisterUser();
-        // }, 5000);
-    }
-
-    setPage(page: pages.Page): void {
-        this.page = page;
-    }
-
-    // ParleyListener
-
-    onConfigureFailed() {
-        console.log('onConfigureFailed');
-    }
-
-    onConfigureSuccess() {
-        console.log('onConfigureSuccess');
-        Parley.getInstance().registerDevice();
-    }
-
-    onDeviceRegistrationFailed() {
-        console.log('onDeviceRegistrationFailed');
-    }
-
-    onDeviceRegistrationSuccess() {
-        console.log('onDeviceRegistrationSuccess');
-
-        this.set("loading", false);
-        let parleyView: ParleyView = this.page.getViewById("parleyView");
-        parleyView.showChat();
-    }
-
-    onUserUnauthorized() {
-        console.log('onUserUnauthorized');
-    }
-
-    onChatMessageSend() {
-        console.log('onChatMessageSend');
-    }
-
-    // ParleySslPinningListener (Optional)
-
-    onEnableSslPinningFailed(message: string): void {
-        console.log('onEnableSslPinningFailed: ' + message);
-    }
-
-    onEnableSslPinningSuccess(): void {
-        console.log('onEnableSslPinningSuccess');
-    }
+    initialised = true;
 }
---
+++ demo/app/main-view-model.ts
@@ -0,0 +1,68 @@
+import {Parley, ParleyListener, ParleySslPinningListener, ParleyView} from "nativescript-parley";
+import {Observable} from "tns-core-modules/data/observable";
+
+export class MainViewModel extends Observable implements ParleyListener, ParleySslPinningListener {
+
+    private parleyView: ParleyView;
+
+    constructor() {
+        super();
+
+        this.set("loading", true);
+
+        Parley.init(this, "0W4qcE5aXoKq9OzvHxj2");
+        Parley.getInstance().registerUser("ZGFhbnw5ZTA5ZjQ2NWMyMGNjYThiYjMxNzZiYjBhOTZmZDNhNWY0YzVlZjYzMGVhNGZmMWUwMjFjZmE0NTEyYjlmMDQwYTJkMTJmNTQwYTE1YmUwYWU2YTZjNTc4NjNjN2IxMmRjODNhNmU1ODNhODhkMmQwNzY2MGYxZTEzZDVhNDk1Mnw1ZDcwZjM5ZTFlZWE5MTM2YmM3MmIwMzk4ZDcyZjEwNDJkNzUwOTBmZmJjNDM3OTg5ZWU1MzE5MzdlZDlkYmFmNTU1YTcyNTUyZWEyNjllYmI5Yzg5ZDgyZGQ3MDYwYTRjZGYxMzE3NWJkNTUwOGRhZDRmMDA1MTEzNjlkYjkxNQ"); // Optional
+        Parley.getInstance().setBaseUrl("https://www.irischat.com"); // Optional
+        Parley.getInstance().setBasePath("/clientApi/v1.1/"); // Optional
+        Parley.getInstance().setOfflineMessagingEnabled(false); // Optional
+        Parley.getInstance().enableSslPinning(this, "N9YyJf13LbHgGv1kn9/YEXGFLJbleikrcpDORa896ok=", "aR6DUqN8qK4HQGhBpcDLVnkRAvOHH1behpQUU1Xl7fE="); // Optional
+        Parley.getInstance().configure();
+
+        // Example for deregister for example, when using a logout button
+        // setTimeout(function () {
+        //     Parley.getInstance().deregisterUser();
+        // }, 5000);
+    }
+
+    setParleyView(parleyView: ParleyView): void {
+        this.parleyView = parleyView;
+    }
+
+    // ParleyListener
+    onConfigureFailed() {
+        console.log('onConfigureFailed');
+    }
+
+    onConfigureSuccess() {
+        console.log('onConfigureSuccess');
+        Parley.getInstance().registerDevice();
+    }
+
+    onDeviceRegistrationFailed() {
+        console.log('onDeviceRegistrationFailed');
+    }
+
+    onDeviceRegistrationSuccess() {
+        console.log('onDeviceRegistrationSuccess');
+
+        this.set("loading", false);
+        this.parleyView.showChat();
+    }
+
+    onUserUnauthorized() {
+        console.log('onUserUnauthorized');
+    }
+
+    onChatMessageSend() {
+        console.log('onChatMessageSend');
+    }
+
+    // ParleySslPinningListener (Optional)
+    onEnableSslPinningFailed(message: string): void {
+        console.log('onEnableSslPinningFailed: ' + message);
+    }
+
+    onEnableSslPinningSuccess(): void {
+        console.log('onEnableSslPinningSuccess');
+    }
+}
